0d954503ff682c461aa43539d933a489faff86e0
Ivan Shevchenko <vedagorhors777@yandex.ru>
2025-12-17 21:51:07 +0300

    Todo list with JSON Placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,111 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import styles from './App.module.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+	// const initialTodos = [
+	// 	{ id: 1, title: 'Проснуться и принять душ' },
+	// 	{ id: 2, title: 'Сделать зарядку' },
+	// 	{ id: 3, title: 'Заняться изучением React' },
+	// ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	const [todos, setTodos] = useState([]); // пустой список до загрузки
+	const [newTask, setNewTask] = useState('');
 
-export default App
+	// 1 способ работы с асинхронным кодом и выполением http запросов с помощью хука useEffect
+	// useEffect(() => {
+	// 	fetch('https://jsonplaceholder.typicode.com/todos?_limit=3')
+	// 		.then((todosData) => todosData.json())
+	// 		.then((loadedTodos) => setTodos(loadedTodos));
+	// }, []); // пустой массив зависимостей -> запуск один раз. Пустой массив указывает на этап монтирования
+
+	// 2 способ работы с асинхроныым кодом (используем async/await) и выполением http запросов с помощью хука useEffect
+
+	useEffect(() => {
+		const fetchTodos = async () => {
+			try {
+				// делаю запрос
+				const response = await fetch(
+					'https://jsonplaceholder.typicode.com/todos?_limit=10',
+				);
+				// проверяю статус ответа
+				if (!response.ok) {
+					throw new Error('Не удалось загрузить задачи');
+				}
+				// Преобразую в JSON
+				const loadedTodos = await response.json();
+
+				// 4. Сохраняю в состояние
+				setTodos(loadedTodos);
+			} catch (error) {
+				// 5. Обрабатываю ошибку
+				console.error('Ошибка при загрузке задач:', error);
+			}
+		};
+
+		fetchTodos();
+	}, []);
+
+	const handleNewTaskChange = (event) => {
+		const value = event.target.value;
+		setNewTask(value);
+		// console.log(value);
+	};
+
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
+		if (newTask.trim() === '') {
+			return;
+		}
+		const newTodo = {
+			id: Date.now(),
+			title: newTask,
+			completed: false,
+		};
+		// Создаю новый массив задач с новой задачей в конце
+		const updateTodos = [...todos, newTodo];
+		setTodos(updateTodos);
+		setNewTask('');
+	};
+
+	return (
+		<main className={styles.todo}>
+			{/* Заголовок */}
+			<h1 className="todo__title">Todo List</h1>
+
+			{/* Форма добавления дела */}
+			<form className="todo__form" onSubmit={handleFormSubmit}>
+				<div className="todo__field field">
+					<label className="field__label" htmlFor="new-task">
+						New task
+					</label>
+					<input
+						className="field__input"
+						id="new-task"
+						placeholder=" "
+						autoComplete="off"
+						value={newTask}
+						onChange={handleNewTaskChange}
+					/>
+				</div>
+				<button className="button" type="submit">
+					Add
+				</button>
+			</form>
+
+			{/* Список дел */}
+			<ul className="todo-app__list">
+				{todos.map((todo) => (
+					<li key={todo.id} className={styles['todo-app__item']}>
+						<input
+							className={styles['checkbox']}
+							type="checkbox"
+							checked={todo.completed}
+							readOnly
+						/>
+						{todo.title}
+					</li>
+				))}
+			</ul>
+		</main>
+	);
+};
